26bb88ce4b68946916e33cd2cbb0ff63d3c75f8b
guiosouza <guilhermedeoliveiradesouza@gmail.com>
2022-12-17 18:52:23 -0300

    Layout /home finished (using breakpoints)

--- frontend/src/pages/Login/index.tsx
+++ frontend/src/pages/Login/index.tsx
@@ -1,8 +1,15 @@
+import { Link } from "react-router-dom";
+import { ReactComponent as BannerImage } from "assets/images/banner.svg";
 import "./styles.css";
 
 const Login = () => {
   return (
     <div className="login-container">
+      <div className="banner-container">
+        <h1>Avalie Filmes</h1>
+        <h2>Diga o que você achou do seu filme favorito</h2>
+        <BannerImage />
+      </div>
       <div className="login-card">
         <h1>LOGIN</h1>
         <form>
@@ -13,7 +20,11 @@ const Login = () => {
             <input type="text" placeholder="Senha" />
           </div>
           <div className="login-submit">
-            <button>FAZER LOGIN</button>
+            <button>
+              <Link to="/">
+                <h2>FAZER LOGIN</h2>
+              </Link>
+            </button>
           </div>
         </form>
       </div>
